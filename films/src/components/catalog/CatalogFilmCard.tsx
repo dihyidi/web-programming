843c1455887bcb979896f184652c0597ff9edffd
dihyidi <dn.koval3@gmail.com>
2021-10-18 11:32:41 +0300

    view more button

--- films/src/components/catalog/CatalogFilmCard.tsx
+++ films/src/components/catalog/CatalogFilmCard.tsx
@@ -1,4 +1,5 @@
 import { Film } from '../../models/film';
+import { Button } from '../button/Button';
 import { FilmInfo } from '../films/FilmInfo';
 import styles from './CatalogFilmCard.module.scss';
 
@@ -11,6 +12,7 @@ export const CatalogFilmCard = (props: CatalogFilmCardProps) => {
         <div>
             <FilmInfo film={props.film} />
             <div className={styles.price}><div><b>Price:</b></div> <div>${price}</div></div>
+            <Button label='View more' />
         </div>
     )
 }
